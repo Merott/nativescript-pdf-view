3f41e48f5395f3adf0dab788ad4bb03d9d933fc1
Merott Movahedi <merott@merott.com>
2016-07-08 21:39:16 +0100

    refactor and get rid of some unnecessary code

--- src/plugin.common.ts
+++ src/plugin.common.ts
@@ -1,30 +1,20 @@
 import * as app from 'application';
-import * as dependencyObservable from 'ui/core/dependency-observable';
+import { Property } from 'ui/core/dependency-observable';
 import * as proxy from 'ui/core/proxy';
 import * as view from 'ui/core/view';
 import * as dialogs from 'ui/dialogs';
 
 export abstract class PDFView extends view.View {
-  private static srcProperty = new dependencyObservable.Property(
-    'src',
-    'PdfView',
-    new proxy.PropertyMetadata('')
-  );
-
-  private static onSrcPropertyChanged(
-    data: dependencyObservable.PropertyChangeData
-  ) {
-    var pdfView = <PDFView>data.object;
-    pdfView.load(data.newValue);
-  }
+  private static srcProperty = new Property(
+    'src', 'PdfView', new proxy.PropertyMetadata(''));
 
   public get src(): string {
     return this._getValue(PDFView.srcProperty);
   }
 
-  public set src(value: string) {
-    this._setValue(PDFView.srcProperty, value);
-    this.load(value);
+  public set src(src: string) {
+    this._setValue(PDFView.srcProperty, src);
+    this.load(src);
   }
 
   public abstract load(src: string);
