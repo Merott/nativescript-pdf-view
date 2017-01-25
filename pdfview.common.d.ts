2c69b8bc083ca3715bae6174bab5470295c6b915
Henk Bakker <spike1292@live.nl>
2017-01-25 13:46:15 +0100

    remove old typing files

--- pdfview.common.d.ts
+++
@@ -1,6 +0,0 @@
-import * as view from 'ui/core/view';
-export declare abstract class PdfView extends view.View {
-    private static srcProperty;
-    src: string;
-    abstract loadPDF(src: string): any;
-}
--- pdfview.ios.d.ts
+++
@@ -1,8 +0,0 @@
-import * as common from './pdfview.common';
-export declare class PdfView extends common.PdfView {
-    private _ios;
-    constructor();
-    ios: any;
-    loadPDF(src: string): void;
-    private init();
-}
